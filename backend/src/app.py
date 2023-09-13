7d2056fd3981c9ce6447b68f85669b10bbce05b9
Carlos Sigua <carlos.sigua@gmail.com>
2023-09-12 23:27:33 -0500

    modificacion simple

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 
@@ -7,6 +7,26 @@ app.config["MONGO_URI"] = "mongodb://192.168.18.31:27017/dbcomments"
 mongoIDB = PyMongo(app)
 
 db = mongoIDB.db.comments
+
+@app.route('/')
+def index():
+    return '<h1>HELO WORLD</h1>'
+
+@app.route('/comments', methods=['POST'])
+def createComment():
+    print(request.json)
+    return 'received'
+
+@app.route('/comments', methods=['GET'])
+def getComments():
+    return 'received'
+
+
+@app.route('/comment/<id>', methods=['GET'])
+def getComment():
+    return 'received'
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
